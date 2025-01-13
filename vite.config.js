eeb8cd248e7c082e02e6de71054a6fada68ca5c5
AskaraSora <kounimation@gmail.com>
2025-01-13 18:47:43 +0700

    Optimize

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.stl'],
+  assetsInclude: ['**/*.glb'],
+  server: {
+    host: true,
+    port: 3000
+  },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three-core': ['three'],
+          'three-libs': ['@react-three/fiber', '@react-three/drei'],
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['three', '@react-three/fiber', '@react-three/drei'],
+    exclude: ['@react-three/postprocessing']
+  }
 })
